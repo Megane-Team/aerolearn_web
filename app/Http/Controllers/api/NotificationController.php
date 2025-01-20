3c260c5aa4dd86ceb9c68f6da9469721f381139c
Raffi14 <Raffifabiansyah@gmail.com>
2025-01-20 14:15:18 +0700

    integrate to api

--- app/Http/Controllers/api/NotificationController.php
+++ app/Http/Controllers/api/NotificationController.php
@@ -9,16 +9,18 @@ use App\Models\Sertifikat;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class NotificationController extends Controller
 {
     public function tambah(Request $request): JsonResponse
     {
+        $tanggal = Carbon::parse($request->tanggal)->format('Y-m-d H:i:s' );
         Notifications::create([
             'id_peserta' => $request->id_peserta,
             'title' => $request->title,
             'detail' => $request->detail,
-            'tanggal' => $request->tanggal,
+            'tanggal' => $tanggal,
             'id_pelaksanaan_pelatihan' => $request->id_pelaksanaan_pelatihan,
         ]);
         return response()->json([
