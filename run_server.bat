75d0433fb42a63f4124ed5d4fcdc6ed7c5fb1fb7
Raffi Fabiansyah <133511291+Raffi14@users.noreply.github.com>
2025-01-31 21:31:06 +0700

    Delete run_server.bat

--- run_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr "IPv4 Address"') do set IP=%%A
-set IP=%IP:~1%
-rem Ganti nilai API_BASE_URL dalam .env
-powershell -Command "(Get-Content .env) -replace 'API_BASE_URL=.*', 'API_BASE_URL=http://%IP%:3000' | Set-Content .env"
-php artisan serve --host=0.0.0.0 --port=8000
